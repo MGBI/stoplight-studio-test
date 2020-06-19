9d7f60fc84dcd508d3dddd54951758d14ca5b47e
Mateusz Grabowski (@pingwin) <grabekm90@users.noreply.github.com>
2020-06-19 17:19:25 +0200

    Set some right panel background color

--- bundle.sh
+++ bundle.sh
@@ -4,7 +4,14 @@ docker build -t redoc-cli -f redoc_cli/Dockerfile .
 # https://github.com/Redocly/redoc#redoc-options-object
 # theme options:
 # https://github.com/Redocly/redoc/blob/master/src/theme.ts
+
+# imsig colors:
+#	--options.theme.colors.primary.main=#3377b2 \
+#	--options.theme.rightPanel.backgroundColor=#3377b2 \
+
 docker run --rm -it  -v $PWD:/data redoc-cli bundle \
 	--options.noAutoAuth \
-	--options.theme.colors.primary.main=black \
+	--options.theme.colors.primary.main=#2C669A \
+	--options.theme.rightPanel.backgroundColor=#2C669A \
+	--options.theme.logo.gutter=20px \
 	/data/reference/krs-rdf/openapi.v1.yaml
