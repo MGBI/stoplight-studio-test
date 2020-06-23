a2d00a8f46e6171baf0f22f7901a8ff92194c45f
Mateusz Grabowski (@pingwin) <grabekm90@users.noreply.github.com>
2020-06-23 16:53:24 +0200

    Bundle to a single YAML file using swagger_cli

--- bundle.sh
+++ bundle.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+docker build -t swagger-cli -f swagger_cli/Dockerfile .
+docker run --rm -it -v $PWD:/data swagger-cli bundle \
+  --dereference \
+  --type yaml \
+  --outfile openapi-bundle.v1.yaml \
+  reference/openapi.v1.yaml
+
 docker build -t redoc-cli -f redoc_cli/Dockerfile .
 # options:
 # https://github.com/Redocly/redoc#redoc-options-object
@@ -9,12 +16,11 @@ docker build -t redoc-cli -f redoc_cli/Dockerfile .
 #	--options.theme.colors.primary.main=#3377b2 \
 #	--options.theme.rightPanel.backgroundColor=#3377b2 \
 
-./clean_spec.py reference/krs-rdf/openapi.v1.yaml > \
-	reference/krs-rdf/.cleaned.yaml
-docker run --rm -it  -v $PWD:/data redoc-cli bundle \
+./clean_spec.py openapi-bundle.v1.yaml > openapi-bundle-cleaned.v1.yaml
+docker run --rm -it -v $PWD:/data redoc-cli bundle \
 	--options.noAutoAuth \
 	--options.theme.colors.primary.main=#2C669A \
 	--options.theme.rightPanel.backgroundColor=#2C669A \
 	--options.theme.logo.gutter=20px \
-	reference/krs-rdf/.cleaned.yaml
-rm -f reference/krs-rdf/.cleaned.yaml
+  openapi-bundle-cleaned.v1.yaml
+rm -f openapi-bundle-cleaned.v1.yaml
