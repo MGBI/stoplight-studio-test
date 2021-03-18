607bc97454a53951c9d230c87d9efd508aa8fc12
Mateusz Grabowski (@pingwin) <grabekm90@users.noreply.github.com>
2021-03-18 23:07:00 +0100

    Set workdir as an output dir for swagger-cli bundle

--- swagger-bundle.sh
+++ swagger-bundle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 sudo chgrp docker-root openapi-bundle.v1.yaml
-docker run --rm -v $PWD:/mnt mgbi/swagger-cli bundle \
+docker run --rm --volume $PWD:/mnt --workdir /mnt mgbi/swagger-cli bundle \
   --dereference \
   --type yaml \
   --outfile openapi-bundle.v1.yaml \
-  /mnt/reference/openapi.v1.yaml
+  reference/openapi.v1.yaml
 
 ./clean_spec.py openapi-bundle.v1.yaml > openapi-bundle-cleaned.v1.yaml
