9728b5304a35fe465629a46719cc9845a07993e3
Mateusz Grabowski (@pingwin) <grabekm90@users.noreply.github.com>
2021-03-18 15:57:56 +0100

    Use mgbi/redoc-cli image

--- bundle.sh
+++ bundle.sh
@@ -6,7 +6,9 @@ docker run --rm -v $PWD:/mnt mgbi/swagger-cli bundle \
   --outfile openapi-bundle.v1.yaml \
   /mnt/reference/openapi.v1.yaml
 
-docker build -t redoc-cli -f redoc_cli/Dockerfile .
+./clean_spec.py openapi-bundle.v1.yaml > openapi-bundle-cleaned.v1.yaml
+sudo chgrp docker-root redoc-static.html
+
 # options:
 # https://github.com/Redocly/redoc#redoc-options-object
 # theme options:
@@ -16,9 +18,7 @@ docker build -t redoc-cli -f redoc_cli/Dockerfile .
 #	--options.theme.colors.primary.main=#3377b2 \
 #	--options.theme.rightPanel.backgroundColor=#3377b2 \
 
-./clean_spec.py openapi-bundle.v1.yaml > openapi-bundle-cleaned.v1.yaml
-sudo chgrp docker-root redoc-static.html
-docker run --rm -it -v $PWD:/mnt redoc-cli bundle \
+docker run --rm -it -v $PWD:/mnt mgbi/redoc-cli bundle \
 	--options.noAutoAuth \
 	--options.theme.colors.primary.main=#2C669A \
 	--options.theme.rightPanel.backgroundColor=#2C669A \
