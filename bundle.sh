568f8b46b636f1dc0892ef79e437ed477b0cd32f
Mateusz Grabowski (@pingwin) <grabekm90@users.noreply.github.com>
2021-03-18 16:39:06 +0100

    Add redoc-serve.sh to serve the ReDoc directly from a bundled file (updated by swagger-bundle.sh)

--- bundle.sh
+++ bundle.sh
@@ -1,27 +1,6 @@
 #!/bin/bash -e
-sudo chgrp docker-root openapi-bundle.v1.yaml
-docker run --rm -v $PWD:/mnt mgbi/swagger-cli bundle \
-  --dereference \
-  --type yaml \
-  --outfile openapi-bundle.v1.yaml \
-  /mnt/reference/openapi.v1.yaml
+./swagger-bundle.sh reference/openapi.v1.yaml
 
-./clean_spec.py openapi-bundle.v1.yaml > openapi-bundle-cleaned.v1.yaml
-sudo chgrp docker-root redoc-static.html
+./redoc-bundle.sh openapi-bundle-cleaned.v1.yaml
 
-# options:
-# https://github.com/Redocly/redoc#redoc-options-object
-# theme options:
-# https://github.com/Redocly/redoc/blob/master/src/theme.ts
-
-# imsig colors:
-#	--options.theme.colors.primary.main=#3377b2 \
-#	--options.theme.rightPanel.backgroundColor=#3377b2 \
-
-docker run --rm -it -v $PWD:/mnt mgbi/redoc-cli bundle \
-	--options.noAutoAuth \
-	--options.theme.colors.primary.main=#2C669A \
-	--options.theme.rightPanel.backgroundColor=#2C669A \
-	--options.theme.logo.gutter=20px \
-  /mnt/openapi-bundle-cleaned.v1.yaml
 rm -f openapi-bundle-cleaned.v1.yaml
---
+++ redoc-serve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+exec docker run --rm -it -v $PWD:/mnt -p 8001:8080 mgbi/redoc-cli serve \
+	--options.noAutoAuth \
+	--options.theme.colors.primary.main=#2C669A \
+	--options.theme.rightPanel.backgroundColor=#2C669A \
+	--options.theme.logo.gutter=20px \
+	--watch \
+  /mnt/$1
