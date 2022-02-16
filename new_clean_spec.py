66b9c743cef81d0f35006f4e13e53abee3f22896
MaksKraj <maksymilian.krajewski@mgbi.pl>
2022-02-16 11:16:09 +0100

    add change

--- new_clean_spec.py
+++ new_clean_spec.py
@@ -17,40 +17,41 @@ def transformation_of_openapi_v2(old_file_path, new_file_path):
         old_openapi = yaml.safe_load(yamlFile)
 
     old_api = {}
-    for path, methods in old_api["paths"].items():
+    for path, methods in old_openapi["paths"].items():
         for method, endpoint in methods.items():
             old_api[method + " " + path.split("msig", 1)[-1]] = endpoint
 
-    # new_openapi = {}
-    # for path, methods in new_openapi["paths"].items():
-    #     for method, endpoint in methods.items():
-    #         new_openapi[method + " " + path.split("msig", 1)[-1]] = endpoint
-    #
-    # for endpoint, new_values in new_openapi.items():
-    #     old_values = old_api.get(endpoint, None)
-    #     if old_values is not None:
-    #         if "description" not in new_values and "description" in old_values:
-    #             new_values["description"] = old_values["description"]
-    #         if "parameters" in new_values:
-    #             for new_item in new_values["parameters"]:
-    #                 new_name = new_item.get("name")
-    #                 for old_item in new_item and old_values["parameters"] or []:
-    #                     if new_name == old_item["name"]:
-    #                         if new_item["schema"].get("description") is None and \
-    #                           old_item["schema"].get(
-    #                           "description") is not None:
-    #                             new_item["schema"]["description"] = old_item[
-    #                               "schema"]["description"]
-    #                     if new_item["schema"].get("example") is None and \
-    #                       old_item["schema"].get("example") is not None:
-    #                         new_item["schema"]["example"] = old_item["schema"][
-    #                           "example"]
-    #                     if new_item.get("description") is None and old_item.get(
-    #                       "description") is not None:
-    #                         new_item["description"] = old_item["description"]
+    new_api = {}
+    for path, methods in new_openapi["paths"].items():
+        for method, endpoint in methods.items():
+            new_api[method + " " + path.split("msig", 1)[-1]] = endpoint
+
+    # here we are taking description and other useful info from old endpoints
+    # to our new
+    for endpoint, new_values in new_api.items():
+        old_values = old_api.get(endpoint, None)
+        if old_values is not None:
+            if "description" not in new_values and "description" in old_values:
+                new_values["description"] = old_values["description"]
+            if "parameters" in new_values:
+                for new_item in new_values["parameters"]:
+                    new_name = new_item.get("name")
+                    for old_item in new_item and old_values["parameters"] or []:
+                        if new_name == old_item["name"]:
+                            if new_item["schema"].get("description") is None \
+                              and old_item["schema"].get(
+                              "description") is not None:
+                                new_item["schema"]["description"] = old_item[
+                                  "schema"]["description"]
+                            if new_item["schema"].get("example") is None and \
+                              old_item["schema"].get("example") is not None:
+                                new_item["schema"]["example"] = old_item["schema"][
+                                  "example"]
+                            if new_item.get("description") is None and old_item.get(
+                              "description") is not None:
+                                new_item["description"] = old_item["description"]
 
     # here we remove admin and internal tags from paths field in new_openapi
-    #
     wanted_paths = defaultdict(dict)
     for path, methods in new_openapi["paths"].items():
         for method, endpoint in methods.items():
@@ -61,7 +62,7 @@ def transformation_of_openapi_v2(old_file_path, new_file_path):
 
     new_openapi["paths"] = wanted_paths
 
-    # here the api version is overwritten in the info field
+    # the api version is overwritten in the info field
     info_section = defaultdict(dict)
     for info, fields in new_openapi["info"].items():
         info_section[info] = fields
