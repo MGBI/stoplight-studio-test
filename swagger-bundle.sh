fab1c48887699c99438a19412735c7f58503b834
Konrad Szymański <ko.szymanski@gmail.com>
2022-02-23 09:28:38 +0100

    improve swagger-bundle.sh

--- swagger-bundle.sh
+++ swagger-bundle.sh
@@ -10,9 +10,8 @@ docker run --rm --volume $PWD:/mnt --workdir /mnt mgbi/swagger-cli bundle \
 wget http://szczur41.mgbi.pl:8080/openapi.json -O iapi.json
 ./merge_spec.py openapi-bundle.yaml iapi.json > openapi-bundle.iapi.yaml
 
-mv reference reference_backup
-mkdir reference
-cp reference_backup/config.yaml reference/config.yaml
+mkdir backup 2> /dev/null && true
+cp -r reference "backup/reference-`date +'%Y-%m-%d_%H-%M-%S'`"
 ./save_spec.py openapi-bundle.iapi.yaml reference/
 
 ./clean_spec.py openapi-bundle.iapi.yaml > openapi-bundle-cleaned.yaml
